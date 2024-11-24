2d4a2d91d549b1c55ba32ac8f06f71c8b01639d8
Rizal-124 <rizalmrgn@gmail.com>
2024-11-24 23:50:26 +0700

    Add files via upload

---
+++ Matematika_Ahmad_Rizal_Arrasyid_1F_24083000124/src/matematika/MatematikaCanggihBeraksi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematika;
+
+/**
+ *
+ * @author Administrator
+ */
+
+public class MatematikaCanggihBeraksi {
+    public static void main(String[] args) {
+        MatematikaCanggih mathop = new MatematikaCanggih();
+        mathop.Matematika(7, 6);
+        mathop.pertambahan();
+        mathop.pengurangan();
+        mathop.perkalian();
+        mathop.sisaBagi();
+    }
+}
